2ede4cf07654cddf24204cea3bbe48c7ec14450b
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-10-18 10:13:58 -0700

    Add crud endpoints

--- src/routes/crud.ts
+++ src/routes/crud.ts
@@ -1,9 +1,11 @@
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import path from 'path'
 import express from 'express'
 
 import { Post } from '../db/schema'
 import { AppContext } from '../types/config'
+import { maybeStr } from '../util/environment'
 
 const jsonParser = bodyParser.json()
 
@@ -57,6 +59,43 @@ const makeRouter = (ctx: AppContext) => {
 
 	router.delete('/posts/:cid', checkKey, async (req: express.Request, res: express.Response) => {
 		await ctx.db.deleteFrom('post').where('post.cid', '=', req.params.cid).execute()
+		return res.sendStatus(200)
+	})
+
+	// Add this endpoint to the makeRouter function
+	router.get('/db/download', checkKey, async (_req: express.Request, res: express.Response) => {
+		const sqlPath = maybeStr(process.env.FEEDGEN_SQLITE_LOCATION)
+		if (!sqlPath) {
+			console.error('No SQLite path configured')
+			return res.status(500).send('Internal Server Error')
+		}
+		const dbPath = path.resolve(__dirname, `../../${sqlPath}`)
+		res.download(dbPath, 'database.sqlite', err => {
+			if (err) {
+				console.error('Error downloading the file:', err)
+				res.status(500).send('Internal Server Error')
+			}
+		})
+	})
+
+	// Add this endpoint to dump posts as CSV
+	router.get('/posts/csv', checkKey, async (_req: express.Request, res: express.Response) => {
+		const posts = await ctx.db.selectFrom('post').selectAll().orderBy('indexedAt', 'desc').execute()
+		const csvHeaders = 'cid,uri,indexedAt\n'
+		const csvRows = posts.map(post => `${post.cid},${post.uri},${post.indexedAt}`).join('\n')
+		const csvData = csvHeaders + csvRows
+
+		res.header('Content-Type', 'text/csv')
+		res.attachment('posts.csv')
+		res.send(csvData)
+	})
+
+	// Add this endpoint to remove posts older than 3 months
+	router.delete('/posts/old', checkKey, async (_req: express.Request, res: express.Response) => {
+		const threeMonthsAgo = new Date()
+		threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3)
+
+		await ctx.db.deleteFrom('post').where('indexedAt', '<', threeMonthsAgo.toISOString()).execute()
 		return res.sendStatus(200)
 	})
 
